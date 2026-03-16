f659580c089ad2d314d3293de51e6300710dc5f7
sachinacharyaa <sachinacharyahancy@gmail.com>
2026-03-16 22:41:44 +0545

    added icon as youtube and twitter

---
+++ frontend/src/component/ui/SidBarItem.tsx
@@ -0,0 +1,10 @@
+import type { ReactElement } from "react";
+
+export function SideBarItem({text, icon}:{
+    text: string,
+    icon: ReactElement;
+}){
+    return <div className="flex">
+        {icon} {text}
+    </div>
+}
