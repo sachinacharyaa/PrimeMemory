e0474a2d78a3fc2ad18c5e7773bf8637a5af0e86
sachinacharyaa <sachinacharyahancy@gmail.com>
2026-03-17 23:20:55 +0545

    working: contentmodel with cross incon and input placeholders

--- frontend/src/component/ui/CreateContentModel.tsx
+++ frontend/src/component/ui/CreateContentModel.tsx
@@ -1,27 +1,48 @@
 // for Add content
 
-import { useState } from "react";
 import { CrossIcon } from "../../Icon/crossIcon";
 
-export function CreateContentModel({ open, onClose }) {
+interface CreateContentModelProps {
+  open: boolean;
+  onClose?: () => void;
+}
+
+export function CreateContentModel({ open, onClose }: CreateContentModelProps) {
   return (
     <div>
       {open && (
         <div
-          className="w-screen h-screen bg-red-200 fixed top-0 left-0 opacity-60
-        justify-center"
+          className="fixed inset-0 bg-black/50 flex items-center justify-center"
         >
-          <div className="flex flex-col justify-center">
-            <span className="bg-white opacity-100 p-4 rounded">
-              {" "}
-              <div className="flex justify-center">
-                <CrossIcon></CrossIcon>
-              </div>
-              <div>{/* /// */}</div>
-            </span>
+          <div className="bg-white p-4 rounded">
+            <div className="flex justify-center">
+              <CrossIcon></CrossIcon>
+            </div>
+            <div className="mt-4 flex flex-col items-center gap-2">
+              <Input placeholder={"Title"}></Input>
+              <Input placeholder={"Link"}></Input>
+            </div>
           </div>
         </div>
       )}
+    </div>
+  );
+}
+
+interface InputProps {
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+}
+
+function Input({ onChange, placeholder }: InputProps) {
+  return (
+    <div>
+      <input
+        placeholder={placeholder}
+        type={"text"}
+        className="px-4 py-2 w-64 text-center"
+        onChange={onChange}
+      ></input>
     </div>
   );
 }
