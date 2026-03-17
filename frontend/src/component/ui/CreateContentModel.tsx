fff7e293719741cf20d36f48c2637f9a950ae4e4
sachinacharyaa <sachinacharyahancy@gmail.com>
2026-03-17 23:08:02 +0545

    added: cross icon top of the CreateContentModel

--- frontend/src/component/ui/CreateContentModel.tsx
+++ frontend/src/component/ui/CreateContentModel.tsx
@@ -1,12 +1,26 @@
 // for Add content
 
 import { useState } from "react";
+import { CrossIcon } from "../../Icon/crossIcon";
 
 export function CreateContentModel({ open, onClose }) {
   return (
     <div>
       {open && (
-        <div className="w-screen h-screen bg-red-200 fixed top-0 left-0 opacity-60"></div>
+        <div
+          className="w-screen h-screen bg-red-200 fixed top-0 left-0 opacity-60
+        justify-center"
+        >
+          <div className="flex flex-col justify-center">
+            <span className="bg-white opacity-100 p-4 rounded">
+              {" "}
+              <div className="flex justify-center">
+                <CrossIcon></CrossIcon>
+              </div>
+              <div>{/* /// */}</div>
+            </span>
+          </div>
+        </div>
       )}
     </div>
   );
