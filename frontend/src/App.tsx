0390ee697d9744e8ede5f87f6644cbd702f826a9
sachinacharyaa <sachinacharyahancy@gmail.com>
2026-03-18 17:11:51 +0545

    fixed icons: X and youtube

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,48 +5,53 @@ import { ShareIcon } from "./Icon/shareIcon";
 import { Card } from "./component/ui/card";
 import { CreateContentModel } from "./component/ui/CreateContentModel";
 import { useState } from "react";
+import { SideBar } from "./component/SideBar";
 
 function App() {
-  const [modelOpen, setModelOpen] = useState(true);
+  const [modelOpen, setModelOpen] = useState(false);
   return (
     <>
-      <CreateContentModel
-        open={modelOpen}
-        onClose={() => {
-          setModelOpen(false);
-        }}
-      />
-      <div className="flex justify-end">
-        <Button
-          onClick={() => {
-            setModelOpen(true);
+      <SideBar></SideBar>
+      {/* Main content */}
+      <div className="p-4 ml-72 h-min-screen bg-gray-100 border-2">
+        <CreateContentModel
+          open={modelOpen}
+          onClose={() => {
+            setModelOpen(false);
           }}
-          variant={"primary"}
-          size="md"
-          title={"Add Content"}
-          startIcon={PlusIcon}
-        ></Button>
-
-        <Button
-          variant={"secondary"}
-          size="md"
-          title={"Share Brain"}
-          startIcon={ShareIcon}
-        ></Button>
-      </div>
-
-      <div className="flex gap-4">
-        <Card
-          type="twitter"
-          link="https://x.com/sachinnacharya/status/2028888621005324351"
-          title="First Tweet"
-        ></Card>
-
-        <Card
-          type="youtube"
-          link="https://youtu.be/BTCdAVWNVcc?si=Dqnls1HsHjbTbY5j"
-          title="First Video"
         />
+        <div className="flex justify-end">
+          <Button
+            onClick={() => {
+              setModelOpen(true);
+            }}
+            variant={"primary"}
+            size="md"
+            title={"Add Content"}
+            startIcon={PlusIcon}
+          ></Button>
+
+          <Button
+            variant={"secondary"}
+            size="md"
+            title={"Share Brain"}
+            startIcon={ShareIcon}
+          ></Button>
+        </div>
+
+        <div className="flex gap-4">
+          <Card
+            type="twitter"
+            link="https://x.com/sachinnacharya/status/2028888621005324351"
+            title="First Tweet"
+          ></Card>
+
+          <Card
+            type="youtube"
+            link="https://youtu.be/BTCdAVWNVcc?si=Dqnls1HsHjbTbY5j"
+            title="First Video"
+          />
+        </div>
       </div>
     </>
   );
--- frontend/src/component/SideBar.tsx
+++ frontend/src/component/SideBar.tsx
@@ -2,14 +2,13 @@ import { Twitter } from "../Icon/x";
 import { Youtube } from "../Icon/Youtube";
 import { SideBarItem } from "./ui/SidBarItem";
 
-
-export function SideBar(){
-    return<div className="h-screen bg-white border-r w-76 fixed left-0 top-0">
-
-        <div className="pt-4">
-            <SideBarItem text = "Twitter" icon = {<Twitter />} />
-            <SideBarItem text = "Youtube" icon = {<Youtube />} />
-
-        </div>
+export function SideBar() {
+  return (
+    <div className="h-screen bg-white border-r w-76 fixed left-0 top-0">
+      <div className="pt-4">
+        <SideBarItem text="Twitter" icon={<Twitter />} />
+        <SideBarItem text="Youtube" icon={<Youtube />} />
+      </div>
     </div>
+  );
 }
