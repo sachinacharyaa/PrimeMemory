8be6c340aea1d1e489f696b73c604dbe570b6e7e
sachinacharyaa <sachinacharyahancy@gmail.com>
2026-03-19 17:25:27 +0545

    Added Type in ContentModel

--- frontend/src/component/ui/CreateContentModel.tsx
+++ frontend/src/component/ui/CreateContentModel.tsx
@@ -3,16 +3,24 @@
 import { CrossIcon } from "../../Icon/crossIcon";
 import { Button } from "./Button";
 import { Input } from "../Input";
-import { useReducer, useRef } from "react";
+import { useReducer, useRef, useState } from "react";
 
 interface CreateContentModelProps {
   open: boolean;
   onClose?: () => void;
 }
 
+//define once and no scope of typo in type
+enum ContenType {
+  Youtube = "youtube",
+  Twitter = "twitter",
+}
+
 export function CreateContentModel({ open, onClose }: CreateContentModelProps) {
   const TitleRef = useRef<HTMLInputElement>();
   const LinkRef = useRef<HTMLInputElement>();
+  const [type, setType] = useState(ContenType.Youtube);
+
   function addContent() {
     const title = TitleRef.current?.value;
     const link = LinkRef.current?.value;
@@ -30,6 +38,30 @@ export function CreateContentModel({ open, onClose }: CreateContentModelProps) {
             <div className="mt-4 flex flex-col items-center gap-2 m-4">
               <Input reference={TitleRef} placeholder={"Title"}></Input>
               <Input reference={LinkRef} placeholder={"Link"}></Input>
+            </div>
+
+            <div>
+              <h1>Type </h1>
+              <div className="flex gap-36 p-4">
+                <Button
+                  title="Youtube"
+                  variant={
+                    type === ContenType.Youtube ? "primary" : "secondary"
+                  }
+                  onClick={() => {
+                    setType(ContenType.Youtube);
+                  }}
+                ></Button>
+                <Button
+                  title="Twitter"
+                  variant={
+                    type === ContenType.Youtube ? "primary" : "secondary"
+                  }
+                  onClick={() => {
+                    setType(ContenType.Twitter);
+                  }}
+                ></Button>
+              </div>
             </div>
             <div className="flex justify-center">
               <Button
