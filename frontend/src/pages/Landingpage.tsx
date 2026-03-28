1a14a0877088e415222e8cca8a6be10cc469502d
sachinacharyaa <sachinacharyahancy@gmail.com>
2026-03-28 17:57:45 +0545

    Added some body content inside body function

--- frontend/src/pages/Landingpage.tsx
+++ frontend/src/pages/Landingpage.tsx
@@ -3,40 +3,61 @@
 // import { Button } from "../component/ui/Button";
 // import { Toggle } from "../Icon/toggle";
 import { Brain } from "../Icon/BrainIcon";
-
+import { useNavigate } from "react-router-dom";
 //Navbar//
 export function LandingPage() {
+  const navigate = useNavigate();
+  function handleSignUp() {
+    navigate("/signup");
+  }
+
   function Navbar() {
     return (
       <header className="w-full bg-white shadow px-6 py-4">
         <div className="flex items-center justify-between">
           <div className="flex text-2xl items-center">
-          <div className="pr-4 text-purple-600 ">
-            <Brain></Brain>
-          </div>
-          <h1> PrimeMemory</h1>
+            <div className="pr-4 text-purple-600 ">
+              <Brain></Brain>
+            </div>
+            <h1> PrimeMemory</h1>
           </div>
 
-        {/* Links */}
-          <div className="flex gap-6 text-gray-700">
+          {/* Links */}
+          <div className="flex items-center gap-6 text-gray-700">
+            {/* Left-side pill */}
+
             <p className="cursor-pointer hover:text-purple-600">Home</p>
             <p className="cursor-pointer hover:text-purple-600">Product</p>
             <p className="cursor-pointer hover:text-purple-600">About</p>
           </div>
 
-        {/* Button */}
-          <button className="bg-purple-600 text-white px-4 py-2 rounded-md">
-            Login
+          {/* Button */}
+          <button
+            className="bg-purple-600 text-white px-4 py-2 rounded-md"
+            onClick={handleSignUp}
+          >
+            Sign Up
           </button>
         </div>
       </header>
     );
   }
+  function Body() {
+    return (
+      <div className="inline-flex items-center gap-2 rounded-full border border-gray-200 bg-white px-4 py-2 shadow-sm">
+        <span className="text-purple-600">✦</span>
+        <span className="text-purple-600 font-medium">
+          Future of Intelligent Memory is Here
+        </span>
+        <span className="text-gray-400">›</span>
+      </div>
+    );
+  }
 
   return (
-    <div className="min-h-screen">
+    <div>
       <Navbar />
-      <main className="p-8"></main>
+      <Body />
     </div>
   );
 }
